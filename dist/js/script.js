fdce06086dcd44dad2a2657eb8853a3bbccbf15f
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-21 14:51:38 -0400

    21.10-dev-20

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,9 +15,9 @@ API.Plugins.tags = {
 				},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.results)){ API.Helper.set(API.Contents,['data','dom','tags',value.name],value); }
+						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','tags',value.name],value); }
 						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','tags',value.name],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.results, {
+						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'TagsIndex',
 							modal:true,
@@ -43,7 +43,7 @@ API.Plugins.tags = {
 					API.request('tags','read',{data:{id:id,key:'name'}},function(result){
 						var dataset = JSON.parse(result);
 						if(dataset.success != undefined){
-							API.GUI.insert(dataset.output.results);
+							API.GUI.insert(dataset.output.dom);
 						}
 					});
 				}
